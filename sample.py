754b5aa74b181a5b0219d3fde245d6d2ce5512ef
tomaslm <tomaslm@hotmail.com>
2020-07-19 10:15:51 -0300

    Initial commit

---
+++ sample.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+import os
+
+import aiohttp
+import sentry_sdk
+from sentry_sdk.integrations.aiohttp import AioHttpIntegration
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+sentry_sdk.init(os.environ["SENTRY_URL"],
+integrations=[AioHttpIntegration()])
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, 'http://python.org')
+        logging.info(f"{len(html)=}")
+
+if __name__ == "__main__":
+    logging.info("starting")
+    asyncio.run(main())
